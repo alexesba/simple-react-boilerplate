323f99876751b55e7f7ad165649916da6a5bfb99
alexesba <alexesba@gmail.com>
2017-01-21 19:19:39 -0600

    added event to load the app when the dom is ready

--- index.js
+++ index.js
@@ -3,5 +3,14 @@ import { render } from "react-dom";
 import Router from "./src/routes";
 import store from "./src/store";
 
-render(Router(store), document.getElementById("App"));
+const run = () => {
+  render(Router(store), document.getElementById("App"));
+}
+
+if (["complete", "loaded", "interactive"].includes(document.readyState) && document.body) {
+  run();
+} else {
+  document.addEventListener("DOMContentLoaded", run, false);
+}
+
 
