db6ef9a69abc259927411b27cce1b972330fd944
alexesba <alexesba@gmail.com>
2017-01-29 12:07:29 -0600

    update styl for 404 pages

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -4,10 +4,19 @@ import { Link } from "react-router";
 import style from "./style.scss";
 
 class NotFound extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  static defaultProps = {
+    message: "not found"
+  }
   render() {
+    const { location: { pathname } } =  this.props;
     return(
       <div styleName="_404">
-        Page not found :(
+        Page: <span> { pathname } </span> {this.props.message }
         <Link to="/"> Home </Link>
       </div>
     );
