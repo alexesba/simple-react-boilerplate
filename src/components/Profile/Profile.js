99141a0e16943e64723f404298df11a20c05cdb2
alexesba <alexesba@gmail.com>
2017-09-09 14:40:50 -0500

    Use the state prop to render the profile info

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,13 +2,22 @@ import React, { Component } from "react";
 
 class Profile extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "Alejandro Espinoza",
+      email: "alexesba@gmail.com",
+      avatar: "https://avatars2.githubusercontent.com/u/579793?v=4&s=460"
+    }
+  }
+
   render() {
     return (
-      <div style={ { maxWidth: "200" } }>
-        <img src="https://avatars2.githubusercontent.com/u/579793?v=4&s=460" width="200"/>
+      <div style={ { maxWidth: 200 } }>
+        <img src={ this.state.avatar } width="200"/>
         <div>
-          <p> Alejandro Espinoza </p>
-          <p> alexesba@gmail.com </p>
+          <p>{ this.state.name }</p>
+          <p>{ this.state.email }</p>
         </div>
       </div>
     );
